10759704176b6d25ce30517b1139dbde708a719a
Olezhantsiy <oleghat@yandex.ru>
2024-09-19 09:41:56 +0300

    lamda

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,12 @@ public class Main {
         Author author = new Author(1, "Pushkin", "Aleksander");
         Publisher publisher = new Publisher(1, "AST", "Sant Petersburg", "www.aaaaa.com");
         Book book = new Book(1, 1, 1, "Evgeniy Onegin", "123-456", 2012, 567, true, "roman(novela)", true);
-        BookProcessor bookProcessor = new BookProcessor() {
-            @Override
-            public void processBook(Book book) {
-                System.out.println("Book: " + book.getTitle());
+
+        BookProcessor bookProcessor = (b) -> {
+                System.out.println("Book: " + b.getTitle());
                 System.out.println("Author: " + author.getFirstName() + " " + author.getLastName());
                 System.out.println("Publisher: " + publisher.getNamePublisher() + ", " + publisher.getAddress());
-                System.out.println("Доступно: " + (book.isStatus() ? "В наличие" : "Отсутствуют"));
-            }
+                System.out.println("Доступно: " + (b.isStatus() ? "В наличие" : "Отсутствуют"));
         };
 
         bookProcessor.processBook(book);
